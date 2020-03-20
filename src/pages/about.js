d84819a7486776d740764d930ec7fa3e0dabe872
ahorihs <ahorihs2@protonmail.ch>
2020-03-19 23:15:29 -0400

    change about

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,10 +5,7 @@ import Layout from '../components/layout';
 export default () => (
   <Layout>
     <h1>About me</h1>
-    <p>
-      I'm working on this site to learn Gatsby. It was forked from Jason
-      Lengstorf's Gatsby workshop on Frontend Masters.
-    </p>
+    <p>I'm working on this site to learn Gatsby.</p>
     <Link to="/">&larr; back to home</Link>
   </Layout>
 );
